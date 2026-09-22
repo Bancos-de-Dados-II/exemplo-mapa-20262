26cf3045e6d2a43e09f51b927b5f187dd624d8da
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-09-22 14:41:18 -0300

    criando rota para retornar o geojson

--- back-end/index.js
+++ back-end/index.js
@@ -13,13 +13,12 @@ const client = await new Client({
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE
 }).connect()
- 
-const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-console.log(res.rows[0].message) // Hello world!
-await client.end()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/municipios/:codigo', async (req, res) => {
+  const geojson = await client.query(
+    'SELECT ST_AsGeoJSON(geom) FROM municipios WHERE id = $1', [req.params.codigo]
+  )
+  res.json(geojson.rows[0]);
 });
 
 app.listen(port, () => {
