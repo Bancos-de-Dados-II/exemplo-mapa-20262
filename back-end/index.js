3f42351b18eab3389c81060f6cf6685701fd21a8
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-09-22 15:05:20 -0300

    Adicionando cors

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import cors from 'cors';
+
 import express from 'express';
 const app = express();
+app.use(cors());
+app.use(express.json());
 const port = 3000;
 
 import { Client } from 'pg'
